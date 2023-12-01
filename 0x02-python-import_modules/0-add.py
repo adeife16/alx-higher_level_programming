4a00b3de24281a274e8c4bb6378ed66312552363
Dada Opeyemi <dadaadeife@gmail.com>
2023-12-01 04:26:15 -0800

    first task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-__import__("add_0")
 
-a = 1
-b = 2
-print(add(a,b))
+if __name__ == "__main__":
+    """Print the summ of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    val = add(a, b)
+    print("{} + {} = {}".format(a, b, val))
